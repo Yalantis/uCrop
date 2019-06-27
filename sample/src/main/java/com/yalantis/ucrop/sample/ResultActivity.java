1a654614c8a7695fa893fce5768e9836a9616e94
Takuma Homma <nagomimatcha@gmail.com>
2019-06-27 18:38:08 +0900

    Migrate sample project to AndroidX

--- sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
+++ sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
@@ -15,16 +15,17 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
 
 import com.yalantis.ucrop.view.UCropView;
 
