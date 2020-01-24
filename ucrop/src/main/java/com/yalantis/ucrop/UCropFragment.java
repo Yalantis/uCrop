740730a1e221b3aa30b6354dcd86e8c695891843
marina.racu <marina.racu@yalantis.com>
2020-01-24 12:26:59 +0200

    Fix vector drawable crash on device < 21 api native

--- ucrop/src/main/java/com/yalantis/ucrop/UCropFragment.java
+++ ucrop/src/main/java/com/yalantis/ucrop/UCropFragment.java
@@ -39,6 +39,7 @@ import androidx.annotation.IdRes;
 import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.transition.AutoTransition;
@@ -93,6 +94,10 @@ public class UCropFragment extends Fragment {
     private Bitmap.CompressFormat mCompressFormat = DEFAULT_COMPRESS_FORMAT;
     private int mCompressQuality = DEFAULT_COMPRESS_QUALITY;
     private int[] mAllowedGestures = new int[]{SCALE, ROTATE, ALL};
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     public static UCropFragment newInstance(Bundle uCrop) {
         UCropFragment fragment = new UCropFragment();
